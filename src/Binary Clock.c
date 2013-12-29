ad0ca4fba1e722b5638fa4d05cbc85f20a2e2bd5
Ken Schmidt <kenschmidt@gmail.com>
2013-12-28 18:54:39 -0800

    Made some testing dot layers

--- src/Binary Clock.c
+++ src/Binary Clock.c
@@ -1,43 +1,77 @@
 #include <pebble.h>
+#include "Binary Clock.h"
+#define NUMROWS 6
+#define NUMCOLS 3
 
+struct Config config;
 static Window *window;
-static TextLayer *text_layer;
+static Layer *dots[NUMROWS][NUMCOLS];
 
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+
+static void drawBackground(struct Layer *layer, GContext *ctx)
+{
+  graphics_context_set_fill_color(ctx, config.background);
+  graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
 }
 
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
+static void drawEnabledDot(struct Layer *layer, GContext *ctx)
+{
+  GRect bounds = layer_get_bounds(layer);
+  int radius = (bounds.size.h < bounds.size.w ? bounds.size.h : bounds.size.w) / 2 - 2 ;
+
+  graphics_context_set_fill_color(ctx, config.foreground);
+  graphics_fill_circle(ctx, (GPoint){.x=bounds.size.w/2, .y=bounds.size.h/2}, radius);
 }
 
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
-}
+static void drawDisabledDot(struct Layer *layer, GContext *ctx)
+{
+  GRect bounds = layer_get_bounds(layer);
+  int strokeSize = 4;
+  int radius = (bounds.size.h < bounds.size.w ? bounds.size.h : bounds.size.w) / 2 - 2 ;
 
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+  graphics_context_set_fill_color(ctx, config.foreground);
+  graphics_fill_circle(ctx, (GPoint){.x=bounds.size.w/2, .y=bounds.size.h/2}, radius);
+  graphics_context_set_fill_color(ctx, config.background);
+  graphics_fill_circle(ctx, (GPoint){.x=bounds.size.w/2, .y=bounds.size.h/2}, radius - strokeSize);
 }
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
+  int height = bounds.size.h / NUMROWS;
+  int width = bounds.size.w / NUMCOLS;
+  GSize dotSize = (GSize){.w = width, .h = height};
+  int i, j;
+  bool enabled = true;
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  layer_set_update_proc(window_layer, drawBackground);
+  for(i=0;i<NUMROWS;i++){
+    for(j=0;j<NUMCOLS;j++){
+      dots[i][j] = layer_create((GRect){.size=dotSize, .origin=(GPoint){.x=j*dotSize.w, .y=i*dotSize.h}});
+      layer_set_clips(dots[i][j], true);
+      if(enabled){
+        layer_set_update_proc(dots[i][j], drawEnabledDot);
+        enabled = false;
+      } else {
+        layer_set_update_proc(dots[i][j], drawDisabledDot);
+        enabled = true;
+      }
+      layer_add_child(window_layer, dots[i][j]);
+    }
+  }
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+  int i, j;
+  for(i=0;i<NUMCOLS;i++){
+    for(j=0;j<NUMROWS;j++)
+      layer_destroy(dots[i][j]);
+  }
 }
 
 static void init(void) {
+  config = (struct Config){.background = GColorBlack, .foreground = GColorWhite};
   window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
---
+++ src/Binary Clock.h
@@ -0,0 +1,8 @@
+#pragma once
+
+struct Config {
+	GColor background;
+	GColor foreground;
+	int numRows;
+	int numCols;
+};
