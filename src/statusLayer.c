230ebe1d27dadaea55921852424fd950e66589ef
Ken Schmidt <kenschmidt@gmail.com>
2014-01-12 23:26:43 -0800

    Changed the battery text to an icon and moved the date over to compensate for the size

--- src/statusLayer.c
+++ src/statusLayer.c
@@ -5,9 +5,8 @@
 
 extern struct Config config;
 
-static BatteryChargeState batteryLevel;
-static TextLayer *batteryTextLayer;
-static char batteryText[16];
+static BitmapLayer *batteryBitmapLayer;
+static InverterLayer *batteryInverter;
 static TextLayer *dateTextLayer;
 static char dateText[16];
 static BitmapLayer *bluetoothLayer;
@@ -23,11 +22,6 @@ void bluetoothConnectionHandler(bool connected) {
 
 static void updateStatus(struct Layer *layer, GContext *ctx)
 {
-  snprintf(batteryText, sizeof(batteryText), "%d%%", batteryLevel.charge_percent);
-  text_layer_set_text_color(batteryTextLayer, config.foreground);
-  text_layer_set_background_color(batteryTextLayer, config.background);
-  text_layer_set_text(batteryTextLayer, batteryText);
-
   time_t now;
   time(&now);
   if(config.date == MMDDYY) {
@@ -39,48 +33,66 @@ static void updateStatus(struct Layer *layer, GContext *ctx)
   text_layer_set_background_color(dateTextLayer, config.background);
   text_layer_set_text(dateTextLayer, dateText);
 
+  layer_set_hidden((Layer *)batteryInverter, config.background);
   layer_set_hidden((Layer *)bluetoothInverter, config.background);
 }
 
 static void updateBatteryStatus(BatteryChargeState charge)
 {
-  memcpy(&batteryLevel, &charge, sizeof(BatteryChargeState));
-  layer_mark_dirty((Layer *)batteryTextLayer);
+  uint32_t batteryLevel;
+  if(charge.is_charging)
+    batteryLevel = 5;
+  else if(charge.charge_percent > 80)
+    batteryLevel = 4;
+  else if(charge.charge_percent > 60)
+    batteryLevel = 3;
+  else if(charge.charge_percent > 40)
+    batteryLevel = 2;
+  else if(charge.charge_percent > 20)
+    batteryLevel = 1;
+  else
+    batteryLevel = 0;
+  GBitmap *baseBitmap = gbitmap_create_with_resource(RESOURCE_ID_BATTERY);
+  bitmap_layer_set_bitmap(batteryBitmapLayer, gbitmap_create_as_sub_bitmap(
+    baseBitmap,
+    (GRect) {
+      .size = (GSize) {
+        .w = baseBitmap->bounds.size.w / 6,
+        .h = baseBitmap->bounds.size.h
+      },
+      .origin = (GPoint) {
+        .x = baseBitmap->bounds.size.w / 6 * batteryLevel,
+        .y = 0
+      }
+    }));
 }
 
 Layer *status_layer_create(GRect bounds)
 {
   Layer *layer = layer_create(bounds);
 
-  batteryTextLayer = text_layer_create((GRect){
-    .size = (GSize){
-      .w = 25,
-      .h = bounds.size.h
+  GSize batterySize = gbitmap_create_with_resource(RESOURCE_ID_BATTERY)->bounds.size;
+  GRect batteryBitmapBounds = (GRect) {
+    .size = (GSize) {
+      .w = batterySize.w / 6,
+      .h = batterySize.h
     },
     .origin = (GPoint){
       .x = 0,
       .y = 0,
     }
-  });
-  BatteryChargeState battery = battery_state_service_peek();
-  memcpy(&batteryLevel, &battery, sizeof(BatteryChargeState));
+  };
+  batteryBitmapLayer = bitmap_layer_create(batteryBitmapBounds);
   battery_state_service_subscribe(updateBatteryStatus);   
-  layer_add_child(layer, (Layer *)batteryTextLayer);
+  updateBatteryStatus(battery_state_service_peek());
+  layer_add_child(layer, (Layer *)batteryBitmapLayer);
 
-  GSize dateSize = graphics_text_layout_get_content_size  ("DDD 00/00/0000",
-    fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD),
-    bounds,
-    GTextOverflowModeTrailingEllipsis,
-    GTextAlignmentCenter
-  );
-  dateTextLayer = text_layer_create((GRect){
-    .size = dateSize,
-    .origin = (GPoint){
-      .x = bounds.size.w / 2 - dateSize.w / 2,
-      .y = 0,
-    }
+  batteryInverter = inverter_layer_create((GRect){
+    .size = batteryBitmapBounds.size,
+    .origin = (GPoint) {.x = 0, .y = 0}
   });
-  layer_add_child(layer, (Layer *)dateTextLayer);
+  layer_add_child((Layer *)batteryBitmapLayer, (Layer *)batteryInverter);
+  layer_set_hidden((Layer *)batteryInverter, config.background);
 
   GRect bluetoothBounds = (GRect){
     .size = (GSize) {.w = 20, .h=20},
@@ -101,6 +113,19 @@ Layer *status_layer_create(GRect bounds)
   });
   layer_add_child((Layer *)bluetoothLayer, (Layer *)bluetoothInverter);
   layer_set_hidden((Layer *)bluetoothInverter, config.background);
+
+  dateTextLayer = text_layer_create((GRect){
+    .size = (GSize) {
+      .h = bounds.size.h,
+      .w = bounds.size.w - batteryBitmapBounds.size.w - bluetoothBounds.size.w
+    },
+    .origin = (GPoint){
+      .x = batteryBitmapBounds.size.w,
+      .y = 0,
+    }
+  });
+  text_layer_set_text_alignment(dateTextLayer, GTextAlignmentCenter);
+  layer_add_child(layer, (Layer *)dateTextLayer);
 
   layer_set_update_proc(layer, updateStatus);
   return layer;
